c0a8a8b1f73567d54ce7082c8d20cf2ed7201df3
srikanth1327 <srikanthrgvm@gmail.com>
2020-09-06 16:46:27 +0530

    denid

--- src/test/java/org/tcs/Github.java
+++ src/test/java/org/tcs/Github.java
@@ -4,6 +4,14 @@ public class Github {
 	
 	public static void main(String[] args) {
 		
+		String s = "hi srikanth";
+		System.out.println(s);
+		System.out.println("done");
+		
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		
 	}
 
 }
